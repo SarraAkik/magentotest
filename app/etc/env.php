c2c48d220a35d724fd0de18591bd13292a2816ee
Akik Sarra <sarraakik@gmail.com>
2024-11-07 21:22:01 +0100

    Initial commit

---
+++ app/etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'nvSRmLEADBQ2fzsVLScaXkjjcvgxm0td'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'b26_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'b26_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base64xNGdatdDC/8s3EYuVY69LlgLpEC3Gg2GgLjLrY4IQ80='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'mage2rock',
+                'username' => 'mage2rock',
+                'password' => 'sarra123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'mage2rock-magento.com'
+    ],
+    'install' => [
+        'date' => 'Wed, 06 Nov 2024 18:07:13 +0000'
+    ]
+];
